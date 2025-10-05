6e5a731e34e11da1ef1185fa5cedd9dfa0a821ae
uwu-gl <moegluwu0@gmail.com>
2025-10-05 10:29:26 +0800

    aston: AOSPA bringup

--- extract-files.py
+++ extract-files.py
@@ -21,7 +21,6 @@ from extract_utils.main import (
 namespace_imports = [
     'vendor/oneplus/sm8550-common',
     'hardware/oplus',
-    'hardware/qcom-caf/sm8550',
     'vendor/qcom/opensource/display',
     'vendor/qcom/opensource/commonsys-intf/display',
 ]
